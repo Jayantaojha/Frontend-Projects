66bb82a2d21bce132b792ef02968b9ed831b188a
Jayanta Ojha <jayantaojha686@gmail.com>
2024-01-07 13:51:39 +0530

    js, images, link added

--- Portfolio/src/script.js
+++ Portfolio/src/script.js
@@ -38,14 +38,13 @@ class Curosal {
             this.itemElements.forEach((item) => {
                 item.classList.add('hidden');
             });
-    
+
             this.itemElements[this.currIndexLeft].classList.remove('hidden');
             this.itemElements[this.currIndexRight].classList.remove('hidden');
         }
 
         // for acivity cards
-        else if(window.innerWidth <= 700 && this.itemElementsArr.length === 3)
-        {
+        else if (window.innerWidth <= 700 && this.itemElementsArr.length === 3) {
 
             this.itemElements.forEach((item) => {
                 item.classList.add('hidden');
@@ -82,3 +81,41 @@ const activityCards = new Curosal(activityIcons, activityItems);
 // --------------------------------------------------------------------------------------------------------------
 
 
+// ++++++++++++++++++++++++++++++++ Peoples says about me section ++++++++++++++++++++++++++++++++++++++++
+
+const peoplesImagesDivs = document.querySelectorAll('.peoples-says-image-div');
+const peoplesSaysPara = document.querySelector('#peoples-says-p');
+
+let currIndex = 0;
+
+function slideImage(){
+    peoplesImagesDivs.forEach((div) => {
+        div.style.display = "none";
+    })
+
+    peoplesImagesDivs[currIndex].style.display = "block";
+
+    switch (currIndex) {
+        case 0:
+            peoplesSaysPara.innerHTML = "<i>Jayanta Ojha is an exceptional software engineer and skilled web developer. <br> His meticulous approach and technical prowess create digital wonders that surpass expectations.</i>"
+            break;
+
+        case 1:
+            peoplesSaysPara.innerHTML = "<i>Meet Jayanta Ojha, a talented software engineer renowned for his expertise in web development. <br>His commitment to innovation and precise coding elevate every project he touches.</i>"
+            break;
+
+        case 2:
+            peoplesSaysPara.innerHTML = "<i>Jayanta Ojha, a standout software engineer and dedicated web developer. <br>His solutions resonate with efficiency and brilliance.</i>"
+            break;
+        
+        default:
+            break;
+    }
+
+    currIndex = (currIndex + 1) % peoplesImagesDivs.length;
+
+}
+
+slideImage();
+
+setInterval(slideImage, 3000);
